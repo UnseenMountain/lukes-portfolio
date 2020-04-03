eb7912f2e68ceafd0e83232e6f1b36aec1267e6c
Luke Klymshyn <unseenmountain@gmail.com>
2020-04-03 00:11:47 -0700

    fixing a bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from "./components/NavBar/NavBar"
