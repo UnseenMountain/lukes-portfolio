4191f4a7ac87e62509bacb4aa8fc0d29452b293f
Luke Klymshyn <unseenmountain@gmail.com>
2020-04-22 20:34:13 -0700

    last few changes

--- src/components/Bio/Bios.js
+++ src/components/Bio/Bios.js
@@ -25,44 +25,44 @@ export default class ReactReveal extends Component {
                   <Card.ImgOverlay>
 
 
-                    <Container >
-                      <Row>
-                        <Rotate>
 
-                          <div style={styles.title}>
+                    <Row>
+                      <Rotate>
+
+                        <div style={styles.title}>
 
 
 
-                            <Fade top big cascade>
+                          <Fade top big cascade>
 
-                              <Container>
-                                <Media as="li">
-                                  <Media.Body>
-                                    <div style={styles.pad} md="auto" className="text-white">
+                            <Container>
+                              <Media >
+                                <Media.Body>
+                                  <div style={styles.pad} md="auto" className="text-white">
 
-                                      <img
-                                        width={200}
-                                        height={200}
-                                        className="align-self-center mr-3"
-                                        src="https://i.ytimg.com/vi/c0NeRZ_unVI/hqdefault.jpg"
-                                        alt="Generic placeholder"
-                                      />
-                                      <h2>Hi, I'm Luke Klymshyn and yes my last name has no vowels.</h2>
-                                      <h2>I'm a UI and UX designer focused on making fun, engaging, and responsive websites for your users.</h2>
+                                    <img
+                                      width={150}
+                                      height={150}
+                                      className="align-self-center mr-3"
+                                      src="https://i.ytimg.com/vi/c0NeRZ_unVI/hqdefault.jpg"
+                                      alt="Generic placeholder"
+                                    />
+                                    <h2>Hi, I'm Luke Klymshyn and yes my last name has no vowels.</h2>
+                                    <h2>I'm a UI and UX designer focused on making fun, engaging, and responsive websites for your users.</h2>
 
-                                    </div>
-                                  </Media.Body>
+                                  </div>
+                                </Media.Body>
 
-                                </Media>
+                              </Media>
 
-                              </Container>
+                            </Container>
 
-                            </Fade>
+                          </Fade>
 
-                          </div>
-                        </Rotate>
-                      </Row>
-                    </Container>
+                        </div>
+                      </Rotate>
+                    </Row>
+
                   </Card.ImgOverlay>
 
                 </Card>
@@ -78,31 +78,31 @@ export default class ReactReveal extends Component {
                 <Card.Img height="750" src="https://data.whicdn.com/images/289974250/original.gif" alt="Card image" />
                 <Card.ImgOverlay>
                   <div style={styles.title}>
-                    <div style={styles.p}>
 
-                      <Jumbotron>
 
-                       
-                          <Zoom bottom cascade duration={1500}>
+                    <Jumbotron>
 
-                            <h5>Luke Klymshyn is a graduate of UCLA and a Fullstack Web Developer who concentrates on UI and UX design.
 
-                            He has worked with HTML5, CSS, JavaScript, Ajax, JSON, API, React bootstrap, Bulma, Databases, firebase, and React.
+                      <Zoom bottom cascade duration={1500}>
+
+                        <h5>Luke Klymshyn is a graduate of UCLA and a Fullstack Web Developer who concentrates on UI and UX design.
+
+                        He has worked with HTML5, CSS, JavaScript, Ajax, JSON, API, React bootstrap, Bulma, Databases, firebase, and React.
 
                             He excels in a group or alone and is focused and dedicated to achieving the goals necessary to complete all goals.</h5>
 
-                           <h5> One of my early achievements was taking second place in the video game design competition at Skills USA with a friend of mine. I have also worked with an international VR company promoting their products at trade shows.
+                        <h5> One of my early achievements was taking second place in the video game design competition at Skills USA with a friend of mine. I have also worked with an international VR company promoting their products at trade shows.
 
-                          My hobbies consist of creative writing, designing video games, and listening to a different collection of music. 
+                        My hobbies consist of creative writing, designing video games, and listening to a different collection of music.
 
                           If you would like to review examples of my work please refer to the gallery below. </h5>
 
-                          </Zoom>
+                      </Zoom>
 
-                        
-                      </Jumbotron>
-                    </div>
+
+                    </Jumbotron>
                   </div>
+
                 </Card.ImgOverlay>
               </Card>
 
@@ -147,11 +147,8 @@ const styles = {
   },
 
   pad: {
-
-    padding: 30
-  },
-  p: {
-
+    margin: 30,
     padding: 80
   },
+
 };
